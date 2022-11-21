e93bebe6d647d04dadae39f27ca2d28755bcd372
Ruchi Chaudhary <chaudharyruchi844@gmail.com>
2022-11-22 03:10:19 +0530

    added signup page

--- contact-maneger-fontend/src/App.js
+++ contact-maneger-fontend/src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Login from './Component/LoginRegistation/Login';
-import SignUp from './Component/LoginRegistation/Signup';
-import TotalContact from './Component/HomeComponent/TotalContact/TotalContact';
-
+import "./App.css";
+import Login from "./Component/LoginRegistation/Login";
+import SignUp from "./Component/LoginRegistation/Signup";
+import TotalContact from "./Component/HomeComponent/TotalContact/TotalContact";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-      <SignUp/>
-      <TotalContact/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<SignUp />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/TotalContact" element={<TotalContact />}></Route>
+      </Routes>
+    </Router>
   );
 }
 

af4068474f096f612b2556cfe5f99eff6a3801e0
Ruchi Chaudhary <chaudharyruchi844@gmail.com>
2022-11-23 22:21:04 +0530

    added login and sign up pages

--- contact-maneger-fontend/src/Component/HomeComponent/TotalContact/TotalContact.js
+++ contact-maneger-fontend/src/Component/HomeComponent/TotalContact/TotalContact.js
@@ -1,18 +1,18 @@
-import ImportCompoent from '../TolalContactComponent/ImportComponent/Import'
-import Delete from '../TolalContactComponent/Delete/Delete';
-import Search from '../TolalContactComponent/Search/Search'
-import Toltip from '../TolalContactComponent/Toltip/TolTip'
+import ImportCompoent from "../TolalContactComponent/ImportComponent/Import";
+import Delete from "../TolalContactComponent/Delete/Delete";
+import Search from "../TolalContactComponent/Search/Search";
+import Toltip from "../TolalContactComponent/Toltip/TolTip";
 
-const TotalContact = ()=>{
-    return (
-        <>
-        hello from TotalContact
-        <ImportCompoent/>
-        <Delete/>
-        <Search/>
-        <Toltip/>
-        </> 
-    )
-}
+const TotalContact = () => {
+  return (
+    <>
+      hello from TotalContact
+      <ImportCompoent />
+      <Delete />
+      <Search />
+      <Toltip />
+    </>
+  );
+};
 
 export default TotalContact;

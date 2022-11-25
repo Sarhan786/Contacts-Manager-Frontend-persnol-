c5ef303634d4ca9be3507fe8145b21b3e7316e78
unknown <pallavikate09@gmail.com>
2022-11-26 00:54:38 +0530

    previous changes

--- contact-maneger-fontend/src/Component/HomeComponent/TolalContactComponent/Toltip/ToltipComponent/Bottoncomponent.js
+++ contact-maneger-fontend/src/Component/HomeComponent/TolalContactComponent/Toltip/ToltipComponent/Bottoncomponent.js
@@ -31,7 +31,7 @@ const Button = () => {
    },[userContactData])
     return(
         <>
-<div style={{dispaly:"grid",girdTemplateColoums:"60% 40%"}}>
+<div style={{dispaly:"grid",girdTemplateColoums:"auto auto"}}>
                   <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tooltip on top"><CalendarMonthIcon/> SelectDate <KeyboardArrowDownIcon size="small" /></button>
                   <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Tooltip on right"><FilterListIcon size="small" /> Filters |<KeyboardArrowDownIcon size="small" /></button>
                 </div>
